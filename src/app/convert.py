be93fbd6707dd0be7b8634ea9a0c51175b69f1a1
Pavel <strbnm@yandex.ru>
2023-01-30 10:46:57 +0300

    Refactored code for add async functionality

--- src/app/convert.py
+++ src/app/convert.py
@@ -1,37 +1,117 @@
-import subprocess
+"""
+Модуль класса конвертера аудио в текст
+"""
 
-import speech_recognition as sr
+import json
 import os
+import subprocess
+from typing import Any
+from urllib.parse import urlencode
 
-from timer import Timer
+import aiohttp
+import speech_recognition as sr
 
 
 class Converter:
+    """Класс конвертера аудио в текст"""
 
-    def __init__(self, path_to_file: str, language: str = "ru-RU"):
-        self.timer = Timer('recognizer_timer')
+    def __init__(self, path_to_file: str, language: str = 'ru-RU'):
         self.language = language
         self.path_to_file = path_to_file
-        self.wav_file = path_to_file.replace(".ogg", ".wav")
-        self.timer.start()
-        subprocess.run(['ffmpeg', '-v', 'quiet', '-i', self.path_to_file, self.wav_file])
-        self.timer.stop()
+        self.wav_file = path_to_file.replace('.ogg', '.wav')
+        subprocess.run(['ffmpeg', '-v', 'quiet', '-i', self.path_to_file, self.wav_file], check=True)
 
-    def audio_to_text(self) -> str:
-        r = sr.Recognizer()
 
+    def prepare_audio(self) -> sr.AudioData:
+        """
+        Метод подготовки аудио к дальнейшему распознаванию
+
+        :return: AudioData
+        """
+        recognizer = sr.Recognizer()
         with sr.AudioFile(self.wav_file) as source:
-            self.timer.start()
-            audio = r.record(source)
-            self.timer.stop()
-            self.timer.start()
-            r.adjust_for_ambient_noise(source)
-            self.timer.stop()
-        self.timer.start()
-        result = r.recognize_google(audio, language=self.language)
-        self.timer.stop()
+            audio = recognizer.record(source)
+            recognizer.adjust_for_ambient_noise(source)
+        return audio
 
-        return result
+    @staticmethod
+    async def recognize_google(
+            session: aiohttp.ClientSession,
+            audio_data: sr.AudioData,
+            key: str | None = None,
+            language: str = 'ru-RU',
+            pfilter: int = 0,
+            show_all: bool = False,
+    ) -> list[Any] | tuple[Any, Any] | Any:
+        """
+        Асинхронная реализация метода распознавания recognize_google с использованием Google Speech Recognition API
+
+        :param session: Экземпляр ClientSession aiohttp
+
+        :param audio_data: Экземпляр AudioData, представляющий моно аудио данные для распознавания
+
+        :param key: API-Key для запроса к Google Speech Recognition API v2. Взят из библиотеки speech_recognition. Не
+        гарантируется что будет работать всегда. Желательно использовать собственный API-Key. Как его получить описано
+        на: `API Keys <http://www.chromium.org/developers/how-tos/api-keys>` page at the Chromium Developers site.
+        In the Google Developers Console, Google Speech Recognition is listed as "Speech API".
+
+        :param language: язык распознавания. Перечень поддерживаемых языков можно найти на `StackOverflow answer
+        <http://stackoverflow.com/a/14302134>`
+
+        :param pfilter: Уровень фильтрации ненормативной лексики. 0 -нет фильтрации, 1 - показывается первая буква,
+        остальные буквы заменяются звездочками.
+
+        :param show_all: флаг, задающий что возвращать в ответе. По умолчанию - False.
+
+        :return: Возвращает наиболее вероятную транскрипцию, если значение `show_all` равно False (по умолчанию).
+        В противном случае возвращает необработанный ответ API в виде словаря JSON.
+        Вызывает исключение `speech_recognition.UnknownValueError`, если речь неразборчива.
+        """
+
+        assert isinstance(audio_data, sr.AudioData), '``audio_data`` must be audio data'
+        assert key is None or isinstance(key, str), '``key`` must be ``None`` or a string'
+        assert isinstance(language, str), '``language`` must be a string'
+
+        flac_data = audio_data.get_flac_data(
+            convert_rate=None if audio_data.sample_rate >= 8000 else 8000,  # audio samples must be at least 8 kHz
+            convert_width=2  # audio samples must be 16-bit
+        )
+        if key is None:
+            key = 'AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw'
+        url_params = urlencode({'client': 'chromium', 'lang': language, 'key': key, 'pFilter': pfilter})
+        url = f'http://www.google.com/speech-api/v2/recognize?{url_params}'
+        async with session.post(
+                url,
+                data=flac_data,
+                headers={'Content-Type': f'audio/x-flac; rate={audio_data.sample_rate}'},
+        ) as response:
+            response_text = await response.text(encoding='utf-8')
+
+        # ignore any blank blocks
+        actual_result = []
+        for line in response_text.split('\n'):
+            if not line:
+                continue
+            result = json.loads(line)['result']
+            if len(result) != 0:
+                actual_result = result[0]
+                break
+
+        # return results
+        if show_all:
+            return actual_result
+        if not isinstance(actual_result, dict) or len(actual_result.get('alternative', [])) == 0:
+            raise sr.UnknownValueError()
+        if 'confidence' in actual_result['alternative']:
+            # return alternative with the highest confidence score
+            best_hypothesis = max(actual_result['alternative'], key=lambda alternative: alternative['confidence'])
+        else:
+            # when there is no confidence available, we arbitrarily choose the first hypothesis.
+            best_hypothesis = actual_result['alternative'][0]
+        if 'transcript' not in best_hypothesis:
+            raise sr.UnknownValueError()
+
+        return best_hypothesis['transcript']
 
     def __del__(self):
         os.remove(self.wav_file)
--- src/app/server.py
+++ src/app/server.py
@@ -1,14 +1,30 @@
+"""
+Модуль приложения асинхронного телеграм-бота для преобразования голосовых сообщений в текстовые.
+"""
+
 import os
 
+import aiohttp
+import speech_recognition as sr
 import telebot
-from telebot.async_telebot import AsyncTeleBot
 from sanic import Sanic, Blueprint, Request, text
 from telebot import types
+from telebot.async_telebot import AsyncTeleBot
 
 from convert import Converter
 from settings.config import settings
 
 app = Sanic(settings.SANIC_APP_NAME)
+
+
+@app.before_server_start
+async def setup_client_session(app, _):
+    app.ctx.client_session = aiohttp.ClientSession()
+
+
+@app.before_server_stop
+async def close_client_session(app, _):
+    await app.ctx.client_session.close()
 
 
 TOKEN = settings.SECURITY.TOKEN
@@ -20,12 +36,30 @@ app.blueprint(v1)
 
 @bot.message_handler(commands=['start'])
 async def start(message: types.Message):
+    """
+    Метод обработчика команды /start
+
+    :param message: объект сообщения Telegram
+    :return: None
+    """
+
     welcome_mess = 'Привет! Отправляй голосовое, я расшифрую!'
     await bot.send_message(message.chat.id, welcome_mess)
 
 
 @bot.message_handler(content_types=['voice', 'video_note', 'audio'])
 async def get_audio_messages(message: types.Message):
+    """
+    Метод обработчика сообщений с контентом типа голосовое сообщение, аудио или видеосообщение.
+
+    Получает аудио или видео файл с голосовым сообщением, осуществляет его преобразование к виду, необходимому
+    для его распознавания с помощью Google Speech Recognition API v2 и отправляет распознанный текст в
+    ответном сообщении Telegram.
+
+    :param message: Объект сообщения Telegram
+    :return: None
+    """
+    file_id = None
     match message.content_type:
         case 'voice':
             file_id = message.voice.file_id
@@ -33,28 +67,49 @@ async def get_audio_messages(message: types.Message):
             file_id = message.audio.file_id
         case 'video_note':
             file_id = message.video_note.file_id
-    file_info = await bot.get_file(file_id)
-    downloaded_file = await bot.download_file(file_info.file_path)
-    file_name = str(message.message_id) + '.ogg'
+    if file_id is not None:
+        file_info = await bot.get_file(file_id)
+        downloaded_file = await bot.download_file(file_info.file_path)
+        file_name = str(message.message_id) + '.ogg'
 
-    with open(file_name, 'wb') as new_file:
-        new_file.write(downloaded_file)
-    converter = Converter(file_name)
-    os.remove(file_name)
-    message_text = converter.audio_to_text()
-    del converter
+        with open(file_name, 'wb') as new_file:
+            new_file.write(downloaded_file)
+        converter = Converter(file_name)
+        os.remove(file_name)
+        audio = converter.prepare_audio()
+        try:
+            message_text = await converter.recognize_google(
+                session=app.ctx.client_session, audio_data=audio, language='ru-RU'
+            )
+        except sr.UnknownValueError:
+            message_text = 'Сообщение не может быть распознано, так как речь не разборчива'
+        except aiohttp.ClientError:
+            message_text = 'Сообщение не может быть распознано. Попробуйте еще раз через несколько секунд'
+        finally:
+            del converter
 
-    await bot.send_message(message.chat.id, message_text, reply_to_message_id=message.message_id)
+        await bot.send_message(message.chat.id, message_text, reply_to_message_id=message.message_id)
 
 
 @v1.post('/')
 async def handler_post(request: Request):
+    """
+    Метод обработчика веб-хук запросов от бота Telegram
+
+    Осуществляет обработку поступающих веб-хук запросов от бота Telegram.
+    Поскольку Telegram не отправляет следующий запрос до получения ответа о получении ранее отправленного,
+    для исключения поведения, когда несколько голосовых сообщений обрабатываются последовательно задачи на обработку
+    голосовых сообщений добавляются как background task и ответ Telegram возвращается немедленно.
+
+    :param request: Экземпляр запроса
+    :return: Экземпляр ответа с текстом '!' в теле ответа
+    """
     json_string = request.body.decode(encoding='utf-8')
     update = telebot.types.Update.de_json(json_string)
-    await bot.process_new_updates([update])
+    request.app.add_task(bot.process_new_updates([update]))
     return text('!')
 
 
 if __name__ == '__main__':
     app.run(host=settings.APP.HOST, port=int(os.environ.get('PORT', 5000)), workers=settings.APP.workers,
-            debug=settings.DEBUG)
+            debug=settings.DEBUG, access_log=True)
